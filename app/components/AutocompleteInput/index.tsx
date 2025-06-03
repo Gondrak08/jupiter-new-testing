9f54d447941a73f9591a0091044693fb409f7b52
=vitor <vitor.alecrim.p@gmail.com>
2025-06-03 13:51:07 -0300

    autoCompleteInput price safari testing

--- app/components/AutocompleteInput/index.tsx
+++ app/components/AutocompleteInput/index.tsx
@@ -1,11 +1,10 @@
 "use client";
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect, useRef, useState } from "react";
+import { axiosInstance } from "@/app/utils";
 import { InputProps } from "./input.types";
 import styles from "./styles.module.scss";
 import { useDebounce } from "@/app/hooks";
 import classNames from "classnames";
-import { axiosInstance } from "@/app/utils";
 
 const AutocompleteInput = ({
   value,
@@ -22,6 +21,9 @@ const AutocompleteInput = ({
   >([]);
   const [emptyError, setEmptyError] = useState<boolean>(false);
 
+  const sessionToken =
+    useRef<google.maps.places.AutocompleteSessionToken | null>(null);
+
   const onFocus = (): void => {
     setShow(true);
   };
@@ -29,16 +31,58 @@ const AutocompleteInput = ({
     setShow(false);
   };
 
+  // const getPlaceDetails = (placeId: string) => {
+  //   console.log("getDetailsCall", placeId);
+  //   const service = new google.maps.places.PlacesService(
+  //     document.createElement("div"),
+  //   );
+  //   console.log("service", service);
+  //   service.getDetails(
+  //     {
+  //       placeId,
+  //       fields: ["geometry", "formatted_address", "name"],
+  //       sessionToken: sessionToken.current!,
+  //     },
+  //     (place, status) => {
+  //       if (
+  //         status === google.maps.places.PlacesServiceStatus.OK &&
+  //         place &&
+  //         place.formatted_address
+  //       ) {
+  //         onChoose({
+  //           ...place,
+  //           description: place.formatted_address,
+  //         } as any); // casting for compatibility
+  //         setSearch(place.formatted_address);
+  //       } else {
+  //         console.error("Error fetching place details:", status);
+  //       }
+  //     },
+  //   );
+  // };
+
   const onItemClick = (
     place: google.maps.places.AutocompletePrediction,
   ): void => {
+    console.log("place-->", place);
     setShow(false);
+    // getPlaceDetails(place.place_id);
 
     onChoose(place);
     setSearch(place.description);
   };
 
   const getPredictions = async () => {
+    console.log("");
+    if (!window.google || !window.google.maps) return;
+    if (!sessionToken.current) {
+      sessionToken.current = new google.maps.places.AutocompleteSessionToken();
+    }
+
+    const service = new google.maps.places.AutocompleteService();
+
+    setLoading(true);
+    setEmptyError(false);
     try {
       setLoading(true);
       setEmptyError(false);
@@ -57,7 +101,28 @@ const AutocompleteInput = ({
       setEmptyError(true);
       console.log(`[getPredictions] error: ${e}`);
     }
+
+    // service.getPlacePredictions(
+    //   {
+    //     input: search,
+    //     sessionToken: sessionToken.current,
+    //     componentRestrictions: { country: "us" }, // Optional
+    //   },
+    //   (predictions, status) => {
+    //     setLoading(false);
+    //     if (
+    //       status === google.maps.places.PlacesServiceStatus.OK &&
+    //       predictions
+    //     ) {
+    //       setResults(predictions);
+    //     } else {
+    //       setEmptyError(true);
+    //       setResults([]);
+    //     }
+    //   },
+    // );
   };
+
   useDebounce(() => {
     if (search && !disabled && search !== value) {
       getPredictions();
@@ -67,6 +132,7 @@ const AutocompleteInput = ({
   useEffect(() => {
     setSearch(value ?? "");
   }, [value]);
+
   return (
     <div className={classNames(styles.input, className)}>
       <input
@@ -92,19 +158,17 @@ const AutocompleteInput = ({
             <div className={styles["select__list-item"]}>No Data</div>
           ) : (
             <div className={styles.select__list}>
-              {results.map((place) => {
-                return (
-                  <div
-                    className={styles["select__list-item"]}
-                    key={place.description}
-                    onMouseDown={() => {
-                      onItemClick(place);
-                    }}
-                  >
-                    {place.description}
-                  </div>
-                );
-              })}
+              {results.map((place) => (
+                <div
+                  className={styles["select__list-item"]}
+                  key={place.place_id}
+                  onMouseDown={() => {
+                    console.log("click", onItemClick(place));
+                  }}
+                >
+                  {place.description}
+                </div>
+              ))}
             </div>
           )}
         </div>
